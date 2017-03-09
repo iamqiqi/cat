c74a913aee126270e8469cb5a77896501fce72e9
Kiki Jiang <itsqiqi@hotmail.com>
2017-03-08 22:48:03 -0800

    change data and responsive code

--- js/main.js
+++ js/main.js
@@ -1,46 +1,43 @@
 $(document).ready(function() {
-  var cat_names = ['cat1', 'cat2', 'cat3'];
-
   var cats = [
-        {
-          clickCount : 0,
-          name : 'cat1',
-          imgSrc : 'images/cat1.jpg',
-          imgAttribution : 'https://www.example.com/iuheweowi'
-        },
-        {
-          clickCount : 0,
-          name : 'cat2',
-          imgSrc : 'images/cat2.jpg',
-          imgAttribution : 'https://www.example.com/uygkkbkinu'
-        },
-        {
-          clickCount : 0,
-          name : 'cat3',
-          imgSrc : 'images/cat3.jpg',
-          imgAttribution : 'https://www.example.com/inugbyugjo'
-        }
-      ];
-  var counts = new Array(cat_names.length).fill(0);
+      {
+        clickCount : 0,
+        name : 'cat1',
+        imgSrc : 'images/cat1.jpg',
+        imgAttribution : 'https://www.example.com/iuheweowi'
+      },
+      {
+        clickCount : 0,
+        name : 'cat2',
+        imgSrc : 'images/cat2.jpg',
+        imgAttribution : 'https://www.example.com/uygkkbkinu'
+      },
+      {
+        clickCount : 0,
+        name : 'cat3',
+        imgSrc : 'images/cat3.jpg',
+        imgAttribution : 'https://www.example.com/inugbyugjo'
+      }
+    ];
 
-  for (let i=0; i < cat_names.length; i++) {
-    let catName = $('<li>').attr('id', 'cat-'+i).text(cat_names[i]).appendTo($('.menu'));
+  for (let i=0; i < cats.length; i++) {
+    let catName = $('<li>').attr('id', 'cat-'+i).text(cats[i].name).appendTo($('.menu'));
 
     catName.on('click', function() {
       $('.content').html("");
       let catWrapper = $('<div>').attr('class', 'cat-wrapper').appendTo($('.content'));
-      let catTitle = $('<h3>').attr('class', 'title').text(cat_names[i]).appendTo(catWrapper);
+      let catTitle = $('<h3>').attr('class', 'title').text(cats[i].name).appendTo(catWrapper);
       let catImg = $('<img>').attr({
-        src: 'images/'+cat_names[i]+'.jpg',
+        src: cats[i].imgSrc,
         class: 'image-responsive',
-        alt: cat_names[i]
+        alt: cats[i].imgAttribution
       }).appendTo(catWrapper);
       let countDiv = $('<div>').appendTo(catWrapper);
       $('<h4>').text('count: ').appendTo(countDiv);
-      let countValueTag = $('<h4>').text(counts[i]).appendTo(countDiv);
+      let countValueTag = $('<h4>').text(cats[i].clickCount).appendTo(countDiv);
       catImg.on('click', function() {
-        counts[i]++;
-        countValueTag.text(counts[i]);
+        cats[i].clickCount++;
+        countValueTag.text(cats[i].clickCount);
       });
     });
   }
